f5588c246a9e2cd8a34e739dea4f43571471e51e
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-08-20 14:17:23 +0530

    Update script_14.js

--- scripts/script_14.js
+++ scripts/script_14.js
@@ -1,9 +1,36 @@
 
 const url_14 = "https://siddh-kivtechs.github.io/menu_kivtechs_json/part_14.json";
-
+let p_json=[{
+                desc: " MAke or Generate your your images using ADOBE SUITE of software",
+                id: 1,
+                icon: "fa-solid fa-broom-ball",
+                image: "https://siddh-kivtechs.github.io/image/fantasy-land-7775979.jpg",
+                title: "ADOBE ILLUSTRATOR"
+            }, {
+                desc: " CANVA for designers ",
+                id: 2,
+                icon: " fa-solid fa-starship-freighter",
+                image: "https://siddh-kivtechs.github.io/image/woman-7838219.jpg",
+                title: "CANVA direct"
+            }, {
+                desc: " AUDIO generator ",
+                id: 3,
+                icon: "fa-solid fa-hat-wizard",
+           
+                image: "https://siddh-kivtechs.github.io/image/cyborg-7956066.jpg",
+                title: "SPEECH SYNTHESIS"
+            }, {
+                desc: " LLM Our take on CHATGPT,with open source models from ORCA and other NLP models ",
+                id: 4,
+                icon: "fa-solid fa-galaxy",
+            
+                image: "https://siddh-kivtechs.github.io/image/artificial-intelligence-7965011.jpg",
+                title: "LLM"
+            }];
 const Movies = () => {
-    const  getMovies = async () => {
-        const p_json=await get_json(url_14);
+     const  getMovies =  () => {
+    // const  getMovies = async () => {
+        // const p_json=await get_json(url_14);
         return p_json.map((movie) => {
             const styles = {
                 backgroundImage: `url(${movie.image})`
