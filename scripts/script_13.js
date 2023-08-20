f2c12844b9cc269e2baeda7fe437c30a2f6db4ac
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-08-20 12:52:51 +0530

    Update script_13.js

--- scripts/script_13.js
+++ scripts/script_13.js
@@ -1,19 +1,9 @@
 
 const url_13 = "https://siddh-kivtechs.github.io/menu_kivtechs_json/part_13.json";
 
-let promise = fetch(url_13);
-
-promise.then(function(response) {
-  // The request was successful.
-  const j_json = response.json();
-  console.log(j_json);
-}).catch(function(error) {
-  // The request failed.
-  console.log(error);
-});
 const Restaurants = () => {
     const getRestaurants = () => {
-        return j_json.map((restaurant) => {
+        return get_json(url_13).map((restaurant) => {
             const styles = {
                 backgroundImage: `url(${restaurant.image})`
             };
