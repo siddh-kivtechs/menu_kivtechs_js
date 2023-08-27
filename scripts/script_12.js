2b199d819b1e0c10931c0f03cd109cd44ff59cb8
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-08-27 10:58:13 +0530

    Update script_12.js

--- scripts/script_12.js
+++ scripts/script_12.js
@@ -68,13 +68,12 @@ let k_json=[{
 
 const Tools = () => {
     const getTools = () => {
-    // const getTools = async () => {
-        // const k_json=await get_json(url_12);
         return k_json.map((tool) => {
             const styles = {
                 backgroundImage: `url(${tool.image})`
             };
                     return (React.createElement("div", { key: tool.id, className: "tool-card" },
+                React.createElement("a", { href: tool.url },
                 React.createElement("div", { className: "tool-card-background background-image", style: styles }),
                 React.createElement("div", { className: "tool-card-content" },
                     React.createElement("div", { className: "tool-card-content-header" },
