d863bdc8ce17fcb1c7dc4a5b416fabe935f62c9c
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-08-27 12:01:24 +0530

    Update script_14.js

--- scripts/script_14.js
+++ scripts/script_14.js
@@ -5,27 +5,31 @@ let p_json=[{
                 id: 1,
                 icon: "fa-solid fa-broom-ball",
                 image: "https://siddh-kivtechs.github.io/image/fantasy-land-7775979.jpg",
-                title: "ADOBE ILLUSTRATOR"
+                title: "ADOBE ILLUSTRATOR",
+                url:""
             }, {
                 desc: " CANVA for designers ",
                 id: 2,
                 icon: " fa-solid fa-starship-freighter",
                 image: "https://siddh-kivtechs.github.io/image/woman-7838219.jpg",
-                title: "CANVA direct"
+                title: "CANVA direct",
+                url:""
             }, {
                 desc: " AUDIO generator ",
                 id: 3,
                 icon: "fa-solid fa-hat-wizard",
            
                 image: "https://siddh-kivtechs.github.io/image/cyborg-7956066.jpg",
-                title: "SPEECH SYNTHESIS"
+                title: "SPEECH SYNTHESIS",
+                url:""
             }, {
                 desc: " LLM Our take on CHATGPT,with open source models from ORCA and other NLP models ",
                 id: 4,
                 icon: "fa-solid fa-galaxy",
             
                 image: "https://siddh-kivtechs.github.io/image/artificial-intelligence-7965011.jpg",
-                title: "LLM"
+                title: "LLM",
+                url:""
             },
             {
                 desc: " LLM",
@@ -49,7 +53,7 @@ const Movies = () => {
                 backgroundImage: `url(${movie.image})`
             };
             const id = `movie-card-${movie.id}`;
-            return (React.createElement("div", { key: movie.id, id: id, className: "movie-card" },
+            return (React.createElement("div", { key: movie.id, id: id, className: "movie-card",href:movie.url },
                 React.createElement("div", { className: "movie-card-background background-image", style: styles }),
                 React.createElement("div", { className: "movie-card-content" },
                     React.createElement("div", { className: "movie-card-info" },
